b47a5f3ca0a579c4052f0545109d9d95a7d06ba8
Juan Carlos Valdés <idevkingos@gmail.com>
2021-09-30 12:28:16 -0300

    feat: jest iniciando con  mocks

--- jest-introduction/src/number-facts/number-facts.test.js
+++ jest-introduction/src/number-facts/number-facts.test.js
@@ -1,7 +1,12 @@
 import { getRandomNumberFact } from "./";
-
+import { getRandomNumberFactService } from './service';
+jest.mock("./service");
 test("should return a random number fact", async () => {
+  getRandomNumberFactService.mockReturnValueOnce({
+    text: "158 is the year that the earliest dated use of Sol invictus, in a dedication from Rome." 
+  });
   const numberFact = await getRandomNumberFact();
+  
   // siempre da error porque la api devuelve datos random entonces debemos usar un mock
   expect(numberFact.text).toBe(
     "158 is the year that the earliest dated use of Sol invictus, in a dedication from Rome."
