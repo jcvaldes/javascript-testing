09046594095b758b4646c82e3a0c0931895f767d
Juan Carlos Valdés <idevkingos@gmail.com>
2021-09-30 12:39:36 -0300

    feat: jest mocks trows exception

--- jest-introduction/src/number-facts/number-facts.test.js
+++ jest-introduction/src/number-facts/number-facts.test.js
@@ -1,14 +1,25 @@
 import { getRandomNumberFact } from "./";
 import { getRandomNumberFactService } from './service';
+
 jest.mock("./service");
+beforeEach(() => {
+  getRandomNumberFactService.mockClear();
+})
 test("should return a random number fact", async () => {
   getRandomNumberFactService.mockReturnValueOnce({
     text: "158 is the year that the earliest dated use of Sol invictus, in a dedication from Rome." 
   });
   const numberFact = await getRandomNumberFact();
   
-  // siempre da error porque la api devuelve datos random entonces debemos usar un mock
   expect(numberFact.text).toBe(
     "158 is the year that the earliest dated use of Sol invictus, in a dedication from Rome."
   );
+  expect(getRandomNumberFactService).toHaveBeenCalledTimes(1);
+});
+test("should return an error when the service throws an exception", async () => {
+  getRandomNumberFactService.mockReturnValueOnce(new Error("Something went wrong"));
+  const numberFact = await getRandomNumberFact();
+  
+  expect(numberFact).toBeInstanceOf(Error);
+  expect(getRandomNumberFactService).toHaveBeenCalledTimes(1);
 });
